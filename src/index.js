7e37bfaa0ca4f71b782d5b7ae4a1755f23d718ff
surajmaru <jakejonathan69@gmail.com>
2025-05-18 15:46:21 +0530

    connect to mongo atlas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-import mongoose from 'mongoose'
-import { DB_NAME } from './constants';
+// syntax
+//require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from './db/index.js';
 
+
+
+
+dotenv.config({
+    path: './env'
+})
+ 
+
+connectDB()
 
 
 
